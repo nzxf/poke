63685696b1c34c57b9be66957fa5e1e244b28904
nzxf <122195155+nzxf@users.noreply.github.com>
2023-05-04 18:24:57 +0700

    Connect abilities and effects API

--- script.js
+++ script.js
@@ -1,10 +1,53 @@
-// randomizer
+// give colors based on type
+const colors = {
+	normal: '#A8A77A',
+	fire: '#EE8130',
+	water: '#6390F0',
+	electric: '#F7D02C',
+	grass: '#7AC74C',
+	ice: '#96D9D6',
+	fighting: '#C22E28',
+	poison: '#A33EA1',
+	ground: '#E2BF65',
+	flying: '#A98FF3',
+	psychic: '#F95587',
+	bug: '#A6B91A',
+	rock: '#B6A136',
+	ghost: '#735797',
+	dragon: '#6F35FC',
+	dark: '#705746',
+	steel: '#B7B7CE',
+	fairy: '#D685AD',
+};
+
+// give random number
 const rand = max => Math.floor(Math.random() * max)
 
-// Axios ==> PokeAPI
+// make number to #000 format
+const digity = function(num) {
+    let x = ["#"]
+    for(let i = 4; i > num.toString().length; i--) {
+        x.push(0)
+    }
+    return x.concat(num).join('')
+}
+
+// capitalize the first letter
+const upFirst = str => str[0].toUpperCase().concat(str.slice(1)); 
+
+// PokeAPI = pokeData
 const pokeData = async (id) => {
     try {
         const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        return res.data
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+const pokeDataAb = async (ability) => {
+    try {
+        const res = await axios.get(`https://pokeapi.co/api/v2/ability/${ability}`)
         return res.data
     } catch (e) {
         console.log(e)
@@ -28,16 +71,23 @@ const chooseOne = async (id = rand(152)) => {
     card.append(leftCard)
 
     // Ability
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < data.abilities.length; i++) {
+        const dataAb = await pokeDataAb(data.abilities[i].ability.name)
+        
         const pokeAbility = document.createElement('P')
         pokeAbility.classList.add('poke-ability')
-        pokeAbility.textContent = "Thunderous roar"
+        pokeAbility.textContent = data.abilities[i].ability.name.toUpperCase()
         leftCard.append(pokeAbility)
         // Effect
         const pokeAbilityEffect = document.createElement('P')
         pokeAbilityEffect.classList.add('poke-ability-effect')
-        pokeAbilityEffect.textContent = "Increases Special Attack to 1.5× but costs 1/8 max HP after each turn during strong sunlight."
+        pokeAbilityEffect.textContent = dataAb.effect_entries[1].effect
         leftCard.append(pokeAbilityEffect)
+        // Short Effect
+        const pokeAbilityShortEffect = document.createElement('P')
+        pokeAbilityShortEffect.classList.add('poke-ability-short-effect')
+        pokeAbilityShortEffect.textContent = dataAb.effect_entries[1].short_effect
+        leftCard.append(pokeAbilityShortEffect)
     }
 
     // Card: middle
@@ -47,7 +97,7 @@ const chooseOne = async (id = rand(152)) => {
 
     const pokeImage = document.createElement('IMG')
     pokeImage.classList.add('poke-image')
-    pokeImage.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/42.png"
+    pokeImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
     midCard.append(pokeImage)
 
     // Card: right-side
@@ -59,16 +109,17 @@ const chooseOne = async (id = rand(152)) => {
     const typeContainer = document.createElement('DIV')
     typeContainer.classList.add('type-container')
     rightCard.append(typeContainer)
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < data.types.length; i++) {
         const pokeType = document.createElement('SPAN')
         pokeType.classList.add('poke-type')
-        pokeType.textContent = "Flying"
+        pokeType.textContent = upFirst(data.types[i].type.name)
+        pokeType.style.backgroundColor = colors[data.types[i].type.name]
         typeContainer.append(pokeType)
     }
     // Name
     const pokeName = document.createElement('P')
     pokeName.classList.add('poke-name')
-    pokeName.textContent = data.name
+    pokeName.textContent = data.name.toUpperCase()
     rightCard.append(pokeName)
     // Id - Height - Weight - Region
     const descContainer = document.createElement('DIV')
@@ -77,26 +128,26 @@ const chooseOne = async (id = rand(152)) => {
 
     const pokeId = document.createElement('SPAN')
     pokeId.classList.add('poke-id')
-    pokeId.textContent = "#006"
+    pokeId.textContent = digity(data.id)
     descContainer.append(pokeId)
     const pokeHeight = document.createElement('SPAN')
     pokeHeight.classList.add('poke-height')
-    pokeHeight.textContent = "170 Cm"
+    pokeHeight.textContent = `${data.height}0 cm`
     descContainer.append(pokeHeight)
     const pokeWeight = document.createElement('SPAN')
     pokeWeight.classList.add('poke-weight')
-    pokeWeight.textContent = "905 Kg"
+    pokeWeight.textContent = `${data.weight/10} kg`
     descContainer.append(pokeWeight)
     const pokeRegion = document.createElement('SPAN')
     pokeRegion.classList.add('poke-region')
-    pokeRegion.textContent = "Kanto"
+    pokeRegion.textContent = "Kanto Region"
     descContainer.append(pokeRegion)
 
     // Stats (hp, att, def, spc-att, spc-def, speed)
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < data.stats.length; i++) {
         const pokeStat = document.createElement('P')
         pokeStat.classList.add('poke-stat')
-        pokeStat.textContent = "Health: 62"
+        pokeStat.textContent = `${upFirst(data.stats[i].stat.name)}: ${data.stats[i].base_stat}`
         rightCard.append(pokeStat)
 
         const pokeBarHolder = document.createElement('DIV')
@@ -105,7 +156,7 @@ const chooseOne = async (id = rand(152)) => {
 
         const pokeBar = document.createElement('DIV')
         pokeBar.classList.add('poke-bar')
-        pokeBar.style.width = `${Math.floor(Math.random() * 100)}%`
+        pokeBar.style.width = `${(data.stats[i].base_stat / 300) * 100}%`
         pokeBarHolder.append(pokeBar)
     }
 
@@ -113,7 +164,7 @@ const chooseOne = async (id = rand(152)) => {
     // Base xp
     const pokeXp = document.createElement('P')
     pokeXp.classList.add('poke-xp')
-    pokeXp.textContent = "XP: 78"
+    pokeXp.textContent = `Base Experience: ${data.base_experience}`
     rightCard.append(pokeXp)
 
     const pokeBarHolderXp = document.createElement('DIV')
@@ -122,8 +173,8 @@ const chooseOne = async (id = rand(152)) => {
 
     const pokeBarXp = document.createElement('DIV')
     pokeBarXp.classList.add('poke-bar')
-    pokeBarXp.style.width = `${Math.floor(Math.random() * 100)}%`
+    pokeBarXp.style.width = `${(data.base_experience / 400) * 100}%`
     pokeBarHolderXp.append(pokeBarXp)
 }
 
-chooseOne()
+chooseOne();
